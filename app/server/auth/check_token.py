9549ec72fc2c04dbaa6e435d87d0fa44ba0f528f
Hamza Ben-Amara <hamza.ben-amara-extern@renault.com>
2021-05-11 03:11:31 +0200

    added ProcFile

--- app/server/auth/check_token.py
+++ app/server/auth/check_token.py
@@ -2,7 +2,7 @@ from fastapi import Request, HTTPException, Header
 from app.server.database.database import *
 
 
-async def _get_authorization_token (customer_id:str  = Header('123456'), api_key:str  = Header('api_key_01'), secret:str  = Header('secret_01')):        
+async def _get_authorization_token (customer_id:str  = Header(...), api_key:str  = Header(...), secret:str  = Header(...)):        
     if customer_id is None:
         print("Failed here.")
         raise HTTPException(status_code=401, detail="The request is missing a valid customer_id")
